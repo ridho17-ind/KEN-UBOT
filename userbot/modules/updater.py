c8ded0ee300c2cbfe2a7a57bfb7792285bd81019
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-10-12 06:40:26 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -208,7 +208,7 @@ async def upstream(event):
             remove("output.txt")
         else:
             await event.edit(changelog_str)
-        return await event.respond('**Perintah Untuk Update, Sebagai Berikut.**\n Command: >`.update now`\n Command: >`.update deploy`\n\n__Untuk Meng Update Fitur SKY-𝐔𝐁𝐎𝐓.__')
+        return await event.respond('**Perintah Untuk Update, Sebagai Berikut.**\n Command: >`.update now`\n Command: >`.update deploy`\n\n__Untuk Meng Update Fitur 𝐒𝐊𝐘-𝐔𝐁𝐎𝐓.__')
 
     if force_update:
         await event.edit(
