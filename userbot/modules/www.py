cd94f1c6c700f0bf7686666ad1f775d3551cf93f
Skyzo <81844427+ridho17-ind@users.noreply.github.com>
2021-10-11 07:08:16 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -206,7 +206,7 @@ async def speedtst(spd):
                    f"`{result['ping']}` \n"
                    "✧ **ISP:** "
                    f"`{result['client']['isp']}` \n"
-                   "✧ **BOT:**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓")
+                   "✧ **BOT:**SKY-𝐔𝐁𝐎𝐓")
 
 
 def speed_convert(size):
